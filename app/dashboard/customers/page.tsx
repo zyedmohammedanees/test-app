8f490ec59521cabfc81adec1f7812c4d3c23454b
zyedmohammedanees <zyedmohammedanees@gmail.com>
2025-08-07 17:57:24 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Customers = () => {
+  return (
+    <div>Customers page</div>
+  )
+}
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoices = () => {
+  return (
+    <div>Invoices page</div>
+  )
+}
+
+export default Invoices
